8f587446e0b9d58d52d011948a496cf4d86aef93
cyclony <yicong.chen@qq.com>
2017-06-03 09:17:29 +0800

    update crawl main class

--- taipingInsure/crawlTaipingData.py
+++ taipingInsure/crawlTaipingData.py
@@ -2,9 +2,13 @@ from bs4 import BeautifulSoup
 import csv
 import requests
 import functools
-
+from concurrent import futures
+from itertools import islice
 
 Price_Item = functools.namedtuple('Price_Item','product_name date product_type buy_price sell_price')
+product_url_prex = 'http://life.cntaiping.com/pricehistory.jspx?a=1&productid={}&pageno={}'
+
+
 
 def get_data(url):
     res = requests.get(url)
@@ -23,17 +27,20 @@ def get_soup(url):
 productid_list_url = 'http://life.cntaiping.com/pricenotice.jspx'
 def get_pid_list(url):
     soup = get_soup(url)
-    return [(tag['value'], tag.text) for tag in soup.select('select option')]
+    return [(tag['value'], tag.text) for tag in soup.select('select option')]  # return 产品id 和产品名称
 
-
-
-product_url_prex = 'http://life.cntaiping.com/pricehistory.jspx?a=1&productid={}&pageno={}'
 
 class Price_Crawler:
     page_no = 1
     prd_id = 0
-    def __init__(self):
-        return
+    prod_name = ''
+    prod_url = ''
+    downloaded_data = []
+
+    def __init__(self, prod_id, prod_name):
+        self.prod_name = prod_name
+        self.prd_id = prod_id
+        self.prod_url = product_url_prex.format(self.prd_id, self.page_no)
 
     def next_page_url(self, url):
         soup = get_soup(url)
@@ -46,28 +53,32 @@ class Price_Crawler:
     def get_prd_his_price_data(self, product_url):
         if product_url:
             soup = get_soup(product_url)
-            for price_item_tag in soup.select('tr'):
-                if price_item_tag.text == '':
-                    continue
-                date, prd_type, buy_price, sell_price, _ = (tag.text.strip() for tag in price_item_tag.find_all('td'))
+            for price_item_tag in islice(soup.select('tr'), 1, 21): #  利用切片函数（islice，将第一条和最后一条过滤掉)
+                date, prd_type, buy_price, sell_price, _ = (tag.text.strip() for tag in price_item_tag.find_all('td'))#  利用unpack将list对应元素赋值
                 yield date, prd_type, buy_price, sell_price
             yield from self.get_prd_his_price_data(self.next_page_url(product_url))
 
-
-    def crawl_price_data(self):
-        for prd_id, prd_name in get_pid_list(productid_list_url):
-            self.prd_id = prd_id
-            self.page_no = 1
-            product_url = product_url_prex.format(self.prd_id, self.page_no)
-            for date, prd_type, buy_price, sell_price in self.get_prd_his_price_data(product_url):
-                yield Price_Item(prd_name, date, prd_type, buy_price, sell_price)
+    def download_prices(self):
+        for date, prd_type, buy_price, sell_price in self.get_prd_his_price_data(self.prod_url):
+            item = Price_Item(self.prod_name, date, prd_type, buy_price, sell_price)
+            print(item)
+            self.downloaded_data.append(item)
+        self.save_to_file()
 
 
-#save to file
-with open('data2.csv', 'a', encoding='utf-8-sig', newline='') as file:
-    c = csv.writer(file)
-    spider = Price_Crawler()
-    for price_item in spider.crawl_price_data():
-        print(price_item)
-        c.writerow(price_item)
-        file.flush()
+    def save_to_file(self):
+        file_name = self.prod_name+'.csv'
+        with open(file_name, 'a', encoding='utf-8-sig', newline='') as file:
+            c = csv.writer(file)
+            c.writerows(self.downloaded_data)
+
+
+def crawl_price_data(tuple):
+    crawler = Price_Crawler(*tuple)
+    crawler.download_prices()
+
+
+prod_id_list = get_pid_list(productid_list_url)
+with futures.ThreadPoolExecutor(5) as executor:
+    res = executor.map(crawl_price_data, prod_id_list)
+    print(len(list(res)))
